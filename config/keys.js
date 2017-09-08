bbeb010e9514c4df666330977e223f4dfc298585
Tristan Southwell <tristan.southwell@hotmail.com>
2017-09-08 16:41:46 +1000

    updates

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: '505751213032-353tmdta7mjn1o57cg11l4itdjthfqfb.apps.googleusercontent.com',
+  googleClientSecret: 'uDVuWUDnZBagctjIWsRbqx1C',
+  mongoURI: 'mongodb://Tristan:pestacide@ds111124.mlab.com:11124/emaily-dev-southy'
+};
